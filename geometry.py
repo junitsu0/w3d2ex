cfe27965191b9fbe89d311bb657ea8276781c5e7
Justin <junitsu00@gmail.com>
2022-07-26 19:46:28 -0500

    ex2 done for real

--- geometry.py
+++ geometry.py
@@ -1,12 +1,10 @@
 from math import pi
 from math import sqrt
 
-r = float(input("Enter the radius of the circle: "))
-print("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
+def radius(r):
+    print(pi * r**2)
 
 
-print("Input lengths of shorter triangle sides:")
-a = float(input("a: "))
-b = float(input("b: "))
-c = sqrt(a**2 + b**2)
-print("The length of the hypotenuse is:", c)
+def hypotenuse(a, b):
+    c = sqrt(a**2 + b**2)
+    print(c)
